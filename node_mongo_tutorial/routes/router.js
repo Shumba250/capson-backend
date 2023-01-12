af2e01fe3b9c80f691a5e8022befa285c6e5b401
Shumba250 <mudakikwaaimable05@gmail.com>
2023-01-12 15:10:48 +0200

    bug{blogs): CRUD of blogs
    created GET API that retrieves the number of blogs in the database
    used JSEND response syntax

--- node_mongo_tutorial/routes/router.js
+++ node_mongo_tutorial/routes/router.js
@@ -5,9 +5,39 @@ import Article from "../models/blogmodules.js";
 router.get("/", async (req, res) => {
 	try {
 		const blogs = await Article.find();
-		res.status(200).json(blogs);
+		res.status(200).json({
+			status: "success",
+			message: "blogs retrieved successfully",
+			data: { blog: blogs },
+		});
 	} catch (error) {
-		res.send({ error: "blogs doesn't exist" });
+		res.status(404).json({ status: "error", message: "blogs not found" });
+	}
+});
+
+router.get("/blogCount", async (req, res) => {
+	try {
+		const blogs = await Article.find().exec();
+		res.status(200).json({
+			status: "success",
+			message: "blogs retrieved successfully",
+			data: { blogs: blogs.length },
+		});
+	} catch (error) {
+		res.status(404).json({ status: "error", message: "blogs not found" });
+	}
+});
+
+router.get("/:id", async (req, res) => {
+	try {
+		const blog = await Article.findOne({ _id: req.body.id });
+		res.status(200).json({
+			status: "success",
+			message: "blog retrieved successfully",
+			data: { blog: blog },
+		});
+	} catch (error) {
+		res.status(404).json({ status: "error", message: "blog not found" });
 	}
 });
 
@@ -19,9 +49,13 @@ router.post("/", async (req, res) => {
 			image: req.body.image,
 		});
 		const blogs = await blog.save();
-		res.status(200).json(blogs);
+		res.status(200).json({
+			status: "success",
+			message: "blog post was a success",
+			data: { blog: blogs },
+		});
 	} catch (error) {
-		res.send({ error: "blog not added" });
+		res.status(404).json({ status: "error", message: "blog not posted" });
 	}
 });
 
@@ -39,18 +73,28 @@ router.patch("/:id", async (req, res) => {
 			blog.image = req.body.image;
 		}
 		const blogs = await blog.save();
-		res.status(200).json(blogs);
+		res.status(200).json({
+			status: "success",
+			message: "blog updated",
+			data: { blog: blogs },
+		});
 	} catch {
-		res.send({ error: "failed to update" });
+		res
+			.status(404)
+			.json({ status: "error", message: "failed to update the blog" });
 	}
 });
 
 router.delete("/:id", async (req, res) => {
 	try {
 		const blog = await Article.deleteOne({ _id: req.params.id });
-		res.status(200).json(blog);
+		res.status(200).json({
+			status: "success",
+			message: "blog deleted",
+			data: { blog: blog },
+		});
 	} catch {
-		res.send({ error: "Post doesn't exist!" });
+		res.status(404).json({ status: "error", message: "blog not deleted" });
 	}
 });
 export default router;
